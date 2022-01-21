1d22bb829ebff565b5b201990a23e607335ac38f
halee99 <577916577@qq.com>
2022-01-21 14:30:17 +0800

    单声道

--- tests/index.js
+++ tests/index.js
@@ -3,7 +3,7 @@ const { HighNotation, LowNotation } = require('./song-of-joy');
 const { AudioCodec } = require('../lib');
 const { pipe } = require('../lib/utils');
 
-// 正弦波双声道欢乐颂
+// 双声道欢乐颂
 const test1 = () => {
   const audioCodec = new AudioCodec({
     samplingRate: 44100,
@@ -23,10 +23,11 @@ const test1 = () => {
       n2.flat(),
     ]),
     ([sam1, sam2]) => audioCodec.dualChannel(sam1, sam2),
-    samples => audioCodec.saveWavByPcm(path.join(__dirname, './audio/1.双声道欢乐颂.wav'), samples),
+    samples => audioCodec.saveWavByPcm(path.join(__dirname, './audio/1.双声道-欢乐颂.wav'), samples),
   )([LowNotation, HighNotation]);
 };
 
+// 谐波双声道欢乐颂
 const test2 = () => {
   const audioCodec = new AudioCodec({
     samplingRate: 44100,
@@ -54,10 +55,11 @@ const test2 = () => {
       n2.flat(),
     ]),
     ([sam1, sam2]) => audioCodec.dualChannel(sam1, sam2),
-    samples => audioCodec.saveWavByPcm(path.join(__dirname, './audio/2.谐波双声道欢乐颂.wav'), samples),
+    samples => audioCodec.saveWavByPcm(path.join(__dirname, './audio/2.谐波-双声道-欢乐颂.wav'), samples),
   )([LowNotation, HighNotation]);
 };
 
+// ADSR双声道
 const test3 = () => {
   const audioCodec = new AudioCodec({
     samplingRate: 44100,
@@ -88,10 +90,11 @@ const test3 = () => {
       n2.flat(),
     ]),
     ([sam1, sam2]) => audioCodec.dualChannel(sam1, sam2),
-    samples => audioCodec.saveWavByPcm(path.join(__dirname, './audio/3.ADSR双声道欢乐颂.wav'), samples),
+    samples => audioCodec.saveWavByPcm(path.join(__dirname, './audio/3.ADSR-双声道-欢乐颂.wav'), samples),
   )([LowNotation, HighNotation]);
 };
 
+// 谐波ADSR双声道
 const test4 = () => {
   const audioCodec = new AudioCodec({
     samplingRate: 44100,
@@ -131,7 +134,52 @@ const test4 = () => {
       n2.flat(),
     ]),
     ([sam1, sam2]) => audioCodec.dualChannel(sam1, sam2),
-    samples => audioCodec.saveWavByPcm(path.join(__dirname, './audio/4.谐波ADSR双声道欢乐颂.wav'), samples),
+    samples => audioCodec.saveWavByPcm(path.join(__dirname, './audio/4.谐波-ADSR-双声道-欢乐颂.wav'), samples),
+  )([LowNotation, HighNotation]);
+};
+
+
+// 谐波ADSR单声道
+const test5 = () => {
+  const audioCodec = new AudioCodec({
+    samplingRate: 44100,
+    samplingBit: 16,
+    channel: 1,
+  });
+
+  // audioCodec.waveConfig(audioCodec.squareWave);
+
+  const ADSR = {
+    A: 0.1,
+    D: 0.2,
+    S: 0.5,
+    sustain: 0.7,
+  };
+
+  pipe(
+    ([n1, n2]) => {
+      const lowSampleGroups = audioCodec.genFrequencyWav(n1);
+      // 谐波
+      const harLowSampleGroups = audioCodec.genFrequencyWav(n1, 3);
+
+      const highSampleGroups = audioCodec.genFrequencyWav(n2);
+      // 谐波
+      const harHighSampleGroups = audioCodec.genFrequencyWav(n2, 3);
+      return [
+        audioCodec.mixSampleGroups(lowSampleGroups, harLowSampleGroups, 0.6, 0.4),
+        audioCodec.mixSampleGroups(highSampleGroups, harHighSampleGroups, 0.6, 0.4),
+      ];
+    },
+    ([n1, n2]) => ([
+      audioCodec.ADSRSynthesizer()(n1, ADSR),
+      audioCodec.ADSRSynthesizer()(n2, ADSR),
+    ]),
+    ([n1, n2]) => ([
+      n1.flat(),
+      n2.flat(),
+    ]),
+    ([n1, n2]) => (audioCodec.mixSamples(n1, n2, 0.5, 0.5)),
+    samples => audioCodec.saveWavByPcm(path.join(__dirname, './audio/5.谐波-ADSR-单声道-欢乐颂.wav'), samples),
   )([LowNotation, HighNotation]);
 };
 
@@ -139,3 +187,4 @@ test1();
 test2();
 test3();
 test4();
+test5();
